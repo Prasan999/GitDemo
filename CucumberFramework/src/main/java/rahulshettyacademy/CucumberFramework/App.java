b5b2259ddd251ffe7afc20b6f788e62266ce8d55
prasanna <prasannakumarpc@outlook.com>
2024-07-24 23:40:12 +0530

    created class in Architect..

--- CucumberFramework/src/main/java/rahulshettyacademy/CucumberFramework/App.java
+++ CucumberFramework/src/main/java/rahulshettyacademy/CucumberFramework/App.java
@@ -1,10 +1,17 @@
 package rahulshettyacademy.CucumberFramework;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Commit Prasanna- from C:\\Users\\pchandrs\\GitDemo workspace");
-    
-    
-  }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Commit Prasanna- from C:\\Users\\pchandrs\\GitDemo workspace");
+
+		System.out.println("Created Develop branch. by .Architetct");
+
+
+	}
+
+	public void architetct() {
+
+		System.out.println("architetct method implementation..");
+	}
 }
