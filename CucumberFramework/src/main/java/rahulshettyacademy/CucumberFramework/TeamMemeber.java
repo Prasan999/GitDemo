6919df4083c604ee080e2e4ba916fde94627a6ab
prasanna <prasannakumarpc@outlook.com>
2024-07-24 23:55:47 +0530

    Created Teammember java class

---
+++ CucumberFramework/src/main/java/rahulshettyacademy/CucumberFramework/TeamMemeber.java
@@ -0,0 +1,11 @@
+package rahulshettyacademy.CucumberFramework;
+
+public class TeamMemeber {
+	
+	public void pushTeammember() {
+		
+		System.out.println("Push from Senior Teamm members..");
+		
+	}
+
+}
